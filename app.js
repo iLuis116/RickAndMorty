134e4f9d8a1914fe702f327bfe6eeebd3635d7ce
Jose Luis Nava Flores <luisflores2103@gmail.com>
2022-10-24 11:51:20 -0500

    Funcion de botones y tarjetas

--- app.js
+++ app.js
@@ -1,22 +1,69 @@
 const urlBase = "https://rickandmortyapi.com/api/character/";
 
-fetch(urlBase)
-  .then((respuesta) => {
-    return respuesta.json();
-  })
-  .then((respJson) => {
-    const info = respJson.info;
-    const personajes = respJson.results;
-    console.log(info);
-    console.log(personajes);
-    showCharacters(personajes);
-  });
-
-  const showCharacters = (personajes) => {
-    const listaPersonajes = document.querySelector('ul');
-    personajes.forEach(personaje =>{
-        const li = document.createElement("li");
-        li.innerText = `Personaje ${personaje.name}`;
-        listaPersonajes.appendChild(li);
+const loadData = (urlBase, page = 1) => {
+  const url = `${urlBase}?page=${page}`;
+  fetch(url)
+    .then((respuesta) => {
+      return respuesta.json();
     })
+    .then((respJson) => {
+      const info = respJson.info;
+      const personajes = respJson.results;
+      console.log(info);
+      //Validar botones
+      const btnPrev = document.querySelector('#prev');
+      const btnNext = document.querySelector('#next');
+      if(!info.prev){
+        btnPrev.classList.add('disabled');
+      } else {
+        btnPrev.classList.remove('disabled');
+        btnPrev.setAttribute('data-id', Number(page) - 1);
+      }
+      if(!info.next){
+        btnNext.classList.add('disabled');
+      } else {
+        btnNext.classList.remove('disabled');
+        btnNext.setAttribute('data-id', Number(page) + 1);
+      }
+      showCharacters(personajes);
+    });
+};
+const showCharacters = (personajes) => {
+  const listaPersonajes = document.querySelector('#characters');
+  while (listaPersonajes.firstChild){
+  listaPersonajes.removeChild(listaPersonajes.firstChild);
   }
+  personajes.forEach((personaje) => {
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.classList.add('m-2');
+    div.innerHTML = creaCard(personaje);
+    listaPersonajes.appendChild(div);
+  });
+};
+
+const creaCard = (personaje) => {
+  const html = `
+  <div class="card" style="width: 18rem;">
+  <img src="${personaje.image}" class="card-img-top" alt="${personaje.image}">
+  <div class="card-body">
+    <h5 class="card-title">${personaje.name}</h5>
+    <p class="card-text">${personaje.status}</p>
+    <a href="#" class="btn btn-primary">Ver más</a>
+  </div>
+</div>
+`;
+return html;
+}
+
+const navegacion = (e)  => {
+  e.preventDefault();
+  if(e.target.classList.contains('btn')){
+    const id = e.target.getAttribute('data-id');
+    loadData(urlBase, id);
+  }
+}
+
+document.querySelector('#botones').addEventListener('click', navegacion);
+
+loadData(urlBase);
